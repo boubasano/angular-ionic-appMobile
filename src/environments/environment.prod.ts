352b3d8b43fbd8d16139d51b0010de46eba31510
boubasano <48012386+boubasano@users.noreply.github.com>
2020-12-07 12:26:50 +0100

    ignored env

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  jsonbin:{
-    key:"$2b$10$2sWyV79LWygzRoaGjWwLO.Ns5HpVsFyfi5LZfxUdcZt.FmWMy012O",
-    url:"https://api.jsonbin.io/b/5fc1030f177c556ef9b3822e",
-  }
-};
